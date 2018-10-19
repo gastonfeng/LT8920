21a883e6fb44fa60d64f28fcf8856dfd6ad7d55e
gastonfeng <gastonfeng@gmail.com>
2018-10-19 16:38:57 +0800

    get id

--- LT8920.cpp
+++ LT8920.cpp
@@ -1,55 +1,55 @@
 #include <SPI.h>
 #include "LT8920.h"
 
-#define REGISTER_READ       0b10000000  //bin
-#define REGISTER_WRITE      0b00000000  //bin
-#define REGISTER_MASK       0b01111111  //bin
+#define REGISTER_READ 0b10000000  //bin
+#define REGISTER_WRITE 0b00000000 //bin
+#define REGISTER_MASK 0b01111111  //bin
 
-#define R_CHANNEL           7
-#define CHANNEL_RX_BIT      7
-#define CHANNEL_TX_BIT      8
-#define CHANNEL_MASK        0b01111111  ///bin
-#define DEFAULT_CHANNEL     0x31
+#define R_CHANNEL 7
+#define CHANNEL_RX_BIT 7
+#define CHANNEL_TX_BIT 8
+#define CHANNEL_MASK 0b01111111 ///bin
+#define DEFAULT_CHANNEL 0x31
 
-#define R_CURRENT           9
+#define R_CURRENT 9
 #define CURRENT_POWER_SHIFT 12
-#define CURRENT_POWER_MASK  0b1111000000000000
-#define CURRENT_GAIN_SHIFT  7
-#define CURRENT_GAIN_MASK   0b0000011110000000
+#define CURRENT_POWER_MASK 0b1111000000000000
+#define CURRENT_GAIN_SHIFT 7
+#define CURRENT_GAIN_MASK 0b0000011110000000
 
-#define R_DATARATE          44
-#define DATARATE_MASK       0xFF00
-#define DATARATE_1MBPS      0x0101
-#define DATARATE_250KBPS    0x0401
-#define DATARATE_125KBPS    0x0801
-#define DATARATE_62KBPS     0x1001
+#define R_DATARATE 44
+#define DATARATE_MASK 0xFF00
+#define DATARATE_1MBPS 0x0101
+#define DATARATE_250KBPS 0x0401
+#define DATARATE_125KBPS 0x0801
+#define DATARATE_62KBPS 0x1001
 
-#define R_SYNCWORD1         36
-#define R_SYNCWORD2         37
-#define R_SYNCWORD3         38
-#define R_SYNCWORD4         39
+#define R_SYNCWORD1 36
+#define R_SYNCWORD2 37
+#define R_SYNCWORD3 38
+#define R_SYNCWORD4 39
 
-#define R_PACKETCONFIG      41
-#define PACKETCONFIG_CRC_ON             0x8000
-#define PACKETCONFIG_SCRAMBLE_ON        0x4000
-#define PACKETCONFIG_PACK_LEN_ENABLE    0x2000
-#define PACKETCONFIG_FW_TERM_TX         0x1000
-#define PACKETCONFIG_AUTO_ACK           0x0800
-#define PACKETCONFIG_PKT_FIFO_POLARITY  0x0400
+#define R_PACKETCONFIG 41
+#define PACKETCONFIG_CRC_ON 0x8000
+#define PACKETCONFIG_SCRAMBLE_ON 0x4000
+#define PACKETCONFIG_PACK_LEN_ENABLE 0x2000
+#define PACKETCONFIG_FW_TERM_TX 0x1000
+#define PACKETCONFIG_AUTO_ACK 0x0800
+#define PACKETCONFIG_PKT_FIFO_POLARITY 0x0400
 
-#define R_STATUS            48
-#define STATUS_CRC_BIT      15
+#define R_STATUS 48
+#define STATUS_CRC_BIT 15
 
+#define R_FIFO 50
+#define R_FIFO_CONTROL 52
 
-#define R_FIFO              50
-#define R_FIFO_CONTROL      52
-
-void LT8920::dump_register(uint8_t reg) {
+void LT8920::dump_register(uint8_t reg)
+{
   uint16_t r = readRegister(reg);
   Serial.print("reg: ");
   Serial.print(reg);
   Serial.print(" , value: ");
-  Serial.println(r,HEX);
+  Serial.println(r, HEX);
 }
 
 LT8920::LT8920(const uint8_t cs, const uint8_t pkt, const uint8_t rst)
@@ -68,73 +68,76 @@ LT8920::LT8920(const uint8_t cs, const uint8_t pkt, const uint8_t rst)
 
 void LT8920::begin()
 {
-    if(_pin_reset > 0)
-    {
-        digitalWrite(_pin_reset, LOW);
-        delay(200);
-        digitalWrite(_pin_reset, HIGH);
-        delay(200);
-
-    }
+  if (_pin_reset > 0)
+  {
+    digitalWrite(_pin_reset, LOW);
+    delay(200);
+    digitalWrite(_pin_reset, HIGH);
+    delay(200);
+  }
   //setup
-
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV64);
+  delay(500);
+  id1 = readRegister(30);
+  id2 = readRegister(31);
   writeRegister(0, 0x6fe0);
   writeRegister(1, 0x5681);
   writeRegister(2, 0x6617);
-  writeRegister(4, 0x9cc9);    //why does this differ from powerup (5447)
-  writeRegister(5, 0x6637);    //why does this differ from powerup (f000)
-  writeRegister(8, 0x6c90);    //power (default 71af) UNDOCUMENTED
+  writeRegister(4, 0x9cc9); //why does this differ from powerup (5447)
+  writeRegister(5, 0x6637); //why does this differ from powerup (f000)
+  writeRegister(8, 0x6c90); //power (default 71af) UNDOCUMENTED
 
-  setCurrentControl(4, 0);     // power & gain.
+  setCurrentControl(4, 0); // power & gain.
 
-  writeRegister(10, 0x7ffd);   //bit 0: XTAL OSC enable
-  writeRegister(11, 0x0000);   //bit 8: Power down RSSI (0=  RSSI operates normal)
+  writeRegister(10, 0x7ffd); //bit 0: XTAL OSC enable
+  writeRegister(11, 0x0000); //bit 8: Power down RSSI (0=  RSSI operates normal)
   writeRegister(12, 0x0000);
-  writeRegister(13, 0x48bd);   //(default 4855)
+  writeRegister(13, 0x48bd); //(default 4855)
 
   writeRegister(22, 0x00ff);
-  writeRegister(23, 0x8005);  //bit 2: Calibrate VCO before each Rx/Tx enable
+  writeRegister(23, 0x8005); //bit 2: Calibrate VCO before each Rx/Tx enable
   writeRegister(24, 0x0067);
   writeRegister(25, 0x1659);
   writeRegister(26, 0x19e0);
-  writeRegister(27, 0x1300);  //bits 5:0, Crystal Frequency adjust
+  writeRegister(27, 0x1300); //bits 5:0, Crystal Frequency adjust
   writeRegister(28, 0x1800);
 
   //fedcba9876543210
-  writeRegister(32, 0x5000);  //AAABBCCCDDEEFFFG  A preamble length, B, syncword length, c trailer length, d packet type
+  writeRegister(32, 0x5000); //AAABBCCCDDEEFFFG  A preamble length, B, syncword length, c trailer length, d packet type
   //                  E FEC_type, F BRCLK_SEL, G reserved
   //0x5000 = 0101 0000 0000 0000 = preamble 010 (3 bytes), B 10 (48 bits)
   writeRegister(33, 0x3fc7);
-  writeRegister(34, 0x2000);  //
-  writeRegister(35, 0x0300);  //POWER mode,  bit 8/9 on = retransmit = 3x (default)
+  writeRegister(34, 0x2000); //
+  writeRegister(35, 0x0300); //POWER mode,  bit 8/9 on = retransmit = 3x (default)
   setSyncWord(0x03805a5a03800380);
 
-  writeRegister(40, 0x4401);  //max allowed error bits = 0 (01 = 0 error bits)
+  writeRegister(40, 0x4401); //max allowed error bits = 0 (01 = 0 error bits)
   writeRegister(R_PACKETCONFIG,
-      PACKETCONFIG_CRC_ON |
-      PACKETCONFIG_PACK_LEN_ENABLE |
-      PACKETCONFIG_FW_TERM_TX
-  );
+                PACKETCONFIG_CRC_ON |
+                    PACKETCONFIG_PACK_LEN_ENABLE |
+                    PACKETCONFIG_FW_TERM_TX);
 
   writeRegister(42, 0xfdb0);
   writeRegister(43, 0x000f);
 
   //setDataRate(LT8920_1MBPS);
 
-  writeRegister(R_FIFO, 0x0000);  //TXRX_FIFO_REG (FIFO queue)
+  writeRegister(R_FIFO, 0x0000); //TXRX_FIFO_REG (FIFO queue)
 
   writeRegister(R_FIFO_CONTROL, 0x8080); //Fifo Rx/Tx queue reset
 
   delay(200);
-  writeRegister(R_CHANNEL, _BV(CHANNEL_TX_BIT));  //set TX mode.  (TX = bit 8, RX = bit 7, so RX would be 0x0080)
+  writeRegister(R_CHANNEL, _BV(CHANNEL_TX_BIT)); //set TX mode.  (TX = bit 8, RX = bit 7, so RX would be 0x0080)
   delay(2);
-  writeRegister(R_CHANNEL, _channel);  // Frequency = 2402 + channel
+  writeRegister(R_CHANNEL, _channel); // Frequency = 2402 + channel
 }
 
 void LT8920::setChannel(uint8_t channel)
 {
   _channel = channel;
-  writeRegister(R_CHANNEL,  (_channel & CHANNEL_MASK));
+  writeRegister(R_CHANNEL, (_channel & CHANNEL_MASK));
 }
 
 uint8_t LT8920::getChannel()
@@ -146,7 +149,7 @@ void LT8920::setCurrentControl(uint8_t power, uint8_t gain)
 {
   writeRegister(R_CURRENT,
                 ((power << CURRENT_POWER_SHIFT) & CURRENT_POWER_MASK) |
-                ((gain << CURRENT_GAIN_SHIFT) & CURRENT_GAIN_MASK));
+                    ((gain << CURRENT_GAIN_SHIFT) & CURRENT_GAIN_MASK));
 }
 
 bool LT8920::setDataRate(DataRate rate)
@@ -155,24 +158,24 @@ bool LT8920::setDataRate(DataRate rate)
 
   switch (rate)
   {
-    case LT8920_1MBPS:
-      newValue = DATARATE_1MBPS;
-      break;
-    case LT8920_250KBPS:
-      newValue = DATARATE_250KBPS;
-      break;
-    case LT8920_125KBPS:
-      newValue = DATARATE_125KBPS;
-      break;
-    case LT8920_62KBPS:
-      newValue = DATARATE_62KBPS;
-      break;
-    default:
-      return false;
+  case LT8920_1MBPS:
+    newValue = DATARATE_1MBPS;
+    break;
+  case LT8920_250KBPS:
+    newValue = DATARATE_250KBPS;
+    break;
+  case LT8920_125KBPS:
+    newValue = DATARATE_125KBPS;
+    break;
+  case LT8920_62KBPS:
+    newValue = DATARATE_62KBPS;
+    break;
+  default:
+    return false;
   }
 
   writeRegister(R_DATARATE, newValue);
-  return ( (readRegister(R_DATARATE) & DATARATE_MASK) == (newValue & DATARATE_MASK));
+  return ((readRegister(R_DATARATE) & DATARATE_MASK) == (newValue & DATARATE_MASK));
 }
 
 LT8920::DataRate LT8920::getDataRate()
@@ -180,14 +183,14 @@ LT8920::DataRate LT8920::getDataRate()
   uint16_t value = readRegister(R_DATARATE) & DATARATE_MASK;
   switch (value)
   {
-    case DATARATE_1MBPS:
-      return LT8920_1MBPS;
-    case DATARATE_250KBPS:
-      return LT8920_250KBPS;
-    case DATARATE_125KBPS:
-      return LT8920_125KBPS;
-    case DATARATE_62KBPS:
-      return LT8920_62KBPS;
+  case DATARATE_1MBPS:
+    return LT8920_1MBPS;
+  case DATARATE_250KBPS:
+    return LT8920_250KBPS;
+  case DATARATE_125KBPS:
+    return LT8920_125KBPS;
+  case DATARATE_62KBPS:
+    return LT8920_62KBPS;
   }
 }
 
@@ -221,7 +224,6 @@ uint8_t LT8920::writeRegister2(uint8_t reg, uint8_t high, uint8_t low)
   // char sbuf[32];
   // sprintf_P(sbuf, PSTR("%d => %02x%02x"), reg, high, low);
   // Serial.println(sbuf);
-
 
   digitalWrite(_pin_chipselect, LOW);
   uint8_t result = SPI.transfer(REGISTER_WRITE | (REGISTER_MASK & reg));
@@ -296,10 +298,10 @@ int LT8920::read(uint8_t *buffer, size_t maxBuffer)
 
     uint16_t data = readRegister(R_FIFO);
     uint8_t packetSize = data >> 8;
-    if(maxBuffer < packetSize+1)
+    if (maxBuffer < packetSize + 1)
     {
-        //BUFFER TOO SMALL
-        return -2;
+      //BUFFER TOO SMALL
+      return -2;
     }
 
     uint8_t pos;
@@ -322,10 +324,10 @@ int LT8920::read(uint8_t *buffer, size_t maxBuffer)
 
 void LT8920::startListening()
 {
-  writeRegister(R_CHANNEL, _channel & CHANNEL_MASK);   //turn off rx/tx
+  writeRegister(R_CHANNEL, _channel & CHANNEL_MASK); //turn off rx/tx
   delay(3);
-  writeRegister(R_FIFO_CONTROL, 0x0080);  //flush rx
-  writeRegister(R_CHANNEL,  (_channel & CHANNEL_MASK) | _BV(CHANNEL_RX_BIT));   //enable RX
+  writeRegister(R_FIFO_CONTROL, 0x0080);                                     //flush rx
+  writeRegister(R_CHANNEL, (_channel & CHANNEL_MASK) | _BV(CHANNEL_RX_BIT)); //enable RX
   delay(5);
 }
 
@@ -335,7 +337,8 @@ void LT8920::setClock(uint8_t clock)
   //register 32, bits 3:1.
   uint16_t val = readRegister(35);
   val &= 0b1111111111110001;
-  val |= ((clock & 0x07) << 1);;
+  val |= ((clock & 0x07) << 1);
+  ;
 
   writeRegister(35, val);
 }
@@ -348,7 +351,7 @@ bool LT8920::sendPacket(uint8_t *data, size_t packetSize)
   }
 
   writeRegister(R_CHANNEL, 0x0000);
-  writeRegister(R_FIFO_CONTROL, 0x8000);  //flush tx
+  writeRegister(R_FIFO_CONTROL, 0x8000); //flush tx
 
   //packets are sent in 16bit words, and the first word will be the packet size.
   //start spitting out words until we are done.
@@ -363,12 +366,12 @@ bool LT8920::sendPacket(uint8_t *data, size_t packetSize)
     writeRegister2(R_FIFO, msb, lsb);
   }
 
-  writeRegister(R_CHANNEL,  (_channel & CHANNEL_MASK) | _BV(CHANNEL_TX_BIT));   //enable TX
+  writeRegister(R_CHANNEL, (_channel & CHANNEL_MASK) | _BV(CHANNEL_TX_BIT)); //enable TX
 
   //Wait until the packet is sent.
   while (digitalRead(_pin_pktflag) == 0)
   {
-      //do nothing.
+    //do nothing.
   }
 
   return true;
@@ -392,37 +395,36 @@ void LT8920::setSyncWordLength(uint8_t option)
 
 uint8_t LT8920::getRSSI()
 {
-    //RSSI: 15:10
-    uint16_t value = readRegister(6);
+  //RSSI: 15:10
+  uint16_t value = readRegister(6);
 
-    return (value >> 10);
+  return (value >> 10);
 }
 
 void LT8920::scanRSSI(uint16_t *buffer, uint8_t start_channel, uint8_t num_channels)
 {
-    // writeRegister(R_CHANNEL, _BV(CHANNEL_RX_BIT));
-    //
-    // //add read mode.
-    writeRegister(R_FIFO_CONTROL, 0x8080);  //flush rx
-    // writeRegister(R_CHANNEL, 0x0000);
+  // writeRegister(R_CHANNEL, _BV(CHANNEL_RX_BIT));
+  //
+  // //add read mode.
+  writeRegister(R_FIFO_CONTROL, 0x8080); //flush rx
+  // writeRegister(R_CHANNEL, 0x0000);
 
-    //set number of channels to scan.
-    writeRegister(42, (readRegister(42) & 0b0000001111111111) | ((num_channels-1 & 0b111111) << 10));
+  //set number of channels to scan.
+  writeRegister(42, (readRegister(42) & 0b0000001111111111) | ((num_channels - 1 & 0b111111) << 10));
 
-    //set channel scan offset.
-    writeRegister(43, (readRegister(43) & 0b0000000011111111) | ((start_channel & 0b1111111) << 8));
-    writeRegister(43, (readRegister(43) & 0b0111111111111111) | _BV(15));
+  //set channel scan offset.
+  writeRegister(43, (readRegister(43) & 0b0000000011111111) | ((start_channel & 0b1111111) << 8));
+  writeRegister(43, (readRegister(43) & 0b0111111111111111) | _BV(15));
 
-    while(digitalRead(_pin_pktflag) == 0)
-    {
-    }
+  while (digitalRead(_pin_pktflag) == 0)
+  {
+  }
 
-
-    //read the results.
-     uint8_t pos = 0;
-    while(pos < num_channels)
-    {
-      uint16_t data = readRegister(R_FIFO);
-      buffer[pos++] = data >> 8;
-    }
+  //read the results.
+  uint8_t pos = 0;
+  while (pos < num_channels)
+  {
+    uint16_t data = readRegister(R_FIFO);
+    buffer[pos++] = data >> 8;
+  }
 }
